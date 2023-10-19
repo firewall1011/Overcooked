e1e951c66989cba4c11dd6b4ac8234e7d9a1a539
Tiago Marino <tmsilva11@gmail.com>
2023-10-19 17:47:06 -0300

    Add initial chef character

---
+++ Source/OvercookedCore/Private/Chef.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2023 Tiago Marino Silva
+
+
+#include "Chef.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+
+AChef::AChef(): InputMapping(nullptr), InputActionMove(nullptr)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AChef::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AChef::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AChef::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+	
+	Subsystem->ClearAllMappings();
+	Subsystem->AddMappingContext(InputMapping, 0);
+
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	Input->BindAction(InputActionMove, ETriggerEvent::Triggered, this, &AChef::Move);
+}
+
+void AChef::Move(const FInputActionValue& InputValue)
+{
+	if (Controller == nullptr)
+		return;
+	
+	const FVector2D MoveValue = InputValue.Get<FVector2D>();
+	const FRotator MovementRotation(0, Controller->GetControlRotation().Yaw, 0);
+ 
+	MoveVertically(MoveValue.Y, MovementRotation);
+	MoveHorizontally(MoveValue.X, MovementRotation);
+}
+
+void AChef::MoveVertically(const double MoveValue, const FRotator& MovementRotation)
+{
+	if (MoveValue != 0.f)
+	{
+		const FVector Direction = MovementRotation.RotateVector(FVector::ForwardVector);
+ 
+		AddMovementInput(Direction, MoveValue);
+	}
+}
+
+void AChef::MoveHorizontally(const double MoveValue, const FRotator& MovementRotation)
+{
+	if (MoveValue != 0.f)
+	{
+		const FVector Direction = MovementRotation.RotateVector(FVector::RightVector);
+	
+		AddMovementInput(Direction, MoveValue);
+	}
+}
+
---
+++ Source/OvercookedCore/Public/Chef.h
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2023 Tiago Marino Silva
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "GameFramework/Character.h"
+#include "Chef.generated.h"
+
+UCLASS()
+class OVERCOOKEDCORE_API AChef : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AChef();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Move(const FInputActionValue& InputValue);
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	UInputAction* InputActionMove;
+
+private:
+	void MoveVertically(const double MoveValue, const FRotator& MovementRotation);
+	void MoveHorizontally(const double MoveValue, const FRotator& MovementRotation);
+};
